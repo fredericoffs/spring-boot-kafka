358011792ebbb9f8ba3a6510a9440e8b70e0e242
Frederico F Silva <fredericoffs@live.com>
2023-07-01 14:37:39 -0300

    LibraryEventsConsumerConfig class

---
+++ library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.learnkafka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+//@EnableKafka
+public class LibraryEventsConsumerConfig {
+}
