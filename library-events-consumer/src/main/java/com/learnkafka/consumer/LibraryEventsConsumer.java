e947369937fff39cb8935e4b4d8304b47783e10d
Frederico F Silva <fredericoffs@live.com>
2023-07-01 15:36:24 -0300

    Build LibraryEvents Kafka Consumer - @KafkaListener Annotation

---
+++ library-events-consumer/src/main/java/com/learnkafka/consumer/LibraryEventsConsumer.java
@@ -0,0 +1,17 @@
+package com.learnkafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LibraryEventsConsumer {
+
+    @KafkaListener(topics = {"library-events"})
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) {
+        log.info("ConsumerRecord: {}", consumerRecord);
+
+    }
+}
